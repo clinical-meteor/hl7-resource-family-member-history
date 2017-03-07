07217ccee16b43fead107ca340c51234a3a437fe
Abigail Watson <awatson1978@gmail.com>
2017-03-07 15:47:55 -0600

    extracted pub/sub functions to clinical:autopublish

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-family-member-history',
-  version: '1.1.1',
+  version: '1.2.0',
   summary: 'HL7 FHIR Resource - Family Member History',
   git: 'https://github.com/clinical-meteor/hl7-resource-family-member-history',
   documentation: 'README.md'
